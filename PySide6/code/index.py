1bfe54c0d05f88a3fd34b6c0e28ee5fddc81efe9
DeltaPrice-Pedro <deltapricepedro@gmail.com>
2024-10-10 14:00:03 -0300

    updt

--- PySide6/code/index.py
+++ PySide6/code/index.py
@@ -1,7 +1,7 @@
 from PySide6.QtWidgets import (QApplication, QMainWindow)
 from PySide6.QtCore import QObject, Signal, QThread
 from PySide6.QtGui import QIcon
-from window import Ui_MainWindow
+from src.window_testes import Ui_MainWindow
 import time
 import sys
 
@@ -12,8 +12,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         self.setWindowIcon((QIcon('cps logo.png')))
 
-        self.radioButton.clicked.connect(
-            lambda: self.set_nivel())
+        self.pushButton.clicked.connect(
+            lambda: self.hard_work())
 
     def hard_work(self):
         self._worker = Worker()
@@ -42,7 +42,7 @@ class Worker(QObject):
         self.started.emit(0)
         for i in range(10,100, 10):
             self.processed.emit(i)
-            time.sleep(2)
+            time.sleep(0.5)
         self.finished.emit(100)
 
 if __name__ == '__main__':
