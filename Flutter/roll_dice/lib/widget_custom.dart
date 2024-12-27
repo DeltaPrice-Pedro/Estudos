2ccb28535e60cd88e829e150e93ac6c08045f167
DeltaPrice-Pedro <deltapricepedro@gmail.com>
2024-12-27 14:03:04 -0300

    Update widget_custom.dart

--- Flutter/roll_dice/lib/widget_custom.dart
+++ Flutter/roll_dice/lib/widget_custom.dart
@@ -4,20 +4,30 @@ class WidgetCustom extends StatelessWidget {
   const WidgetCustom(this.texto, {super.key});
   final String texto;
 
+  void method() {}
+
   @override
   Widget build(context) {
     return Container(
       decoration: BoxDecoration(
           gradient: LinearGradient(colors: [
+        const Color.fromARGB(255, 10, 103, 210),
         const Color.fromARGB(255, 3, 115, 244),
         const Color.fromARGB(255, 182, 215, 253),
-      ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+      ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
       // alignment: Alignment.topCenter,
       child: Center(
-          child: Image.asset(
-        'assets/images/deltaprice_hori.png',
-        width: 400,
-      )),
+          child: Column(children: [
+        Image.asset(
+          'assets/images/deltaprice_hori.png',
+          width: 400,
+        ),
+        TextButton(
+            onPressed: method,
+            style:
+                TextButton.styleFrom(padding: const EdgeInsets.only(top: 20)),
+            child: const Text('Enviar')),
+      ])),
     );
   }
 }
