b52f14a295018fc932c0ae07b1d8449d9e78383d
DeltaPrice-Pedro <deltapricepedro@gmail.com>
2024-12-27 09:36:57 -0300

    Update main.dart

--- Flutter/roll_dice/lib/main.dart
+++ Flutter/roll_dice/lib/main.dart
@@ -1,4 +1,21 @@
+import 'package:flutter/material.dart';
 
-void main(){
-  runApp();
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sesas',
+      home: Scaffold(
+        body: Center(
+          child: Text('Bem vindo Sesas'),
+        ),
+      ),
+    );
+  }
 }
