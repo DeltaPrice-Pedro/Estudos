5311d69b04f0203888580944a4170608f719c31a
DeltaPrice-Pedro <deltapricepedro@gmail.com>
2024-12-30 14:04:24 -0300

    switchPage

--- Flutter/roll_dice/lib/login/login.dart
+++ Flutter/roll_dice/lib/login/login.dart
@@ -1,22 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:roll_dice/login/acess_widget.dart';
 
 class Login extends StatelessWidget {
   const Login({super.key});
 
-  @override
-  Widget build(context) {
+@override
+ Widget build(context) {
     return Column(
       children: [
-        Opacity(
-          opacity: 0.2,
-          child: Image.asset(
-            'assets/images/deltaprice_hori.png',
-            width: 400,
-          ),
+        Text(
+          'Bem vindo(a)\nao Delta Alimentos!',
+          textAlign: TextAlign.center,
         ),
-        Text('Bem vindo(a)\nao Delta Alimentos!'),
-        AcessWidget()
       ],
     );
   }
---
+++ Flutter/roll_dice/lib/menu/menu.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class Menu extends StatefulWidget {
+  const Menu({super.key});
+
+  @override
+  State<Menu> createState() {
+    return _Menu();
+  }
+}
+
+class _Menu extends State<Menu> {
+  String textChange = 'Sesas Num.: Null';
+  var random = Random();
+
+  void method() {
+    int randomNum = random.nextInt(11);
+    setState(() {
+      textChange = 'Sesas Num.: $randomNum';
+    });
+  }
+
+  void switchPage() {
+    setState(() {
+      
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return Column(children: [
+      Text(
+        textChange,
+        style: TextStyle(fontSize: 40),
+      ),
+      TextButton.icon(
+          onPressed: method,
+          style: TextButton.styleFrom(
+              padding: const EdgeInsets.only(top: 20),
+              minimumSize: Size(40, 40)),
+          icon: const Icon(Icons.import_contacts_sharp),
+          label: const Text('Enviar')),
+      TextButton.icon(
+          onPressed: method,
+          style: TextButton.styleFrom(
+              padding: const EdgeInsets.only(top: 20),
+              minimumSize: Size(40, 40)),
+          icon: const Icon(Icons.import_contacts_sharp),
+          label: const Text('Enviar'))
+    ]);
+  }
+}
