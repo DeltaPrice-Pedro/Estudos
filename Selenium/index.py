a70552003af7675eeb1f30140bb71786907987ae
Pedro Henrique Resende Menezes <deltapricepedro@gmail.com>
2024-12-30 17:05:12 -0300

    Update index.py

--- Selenium/index.py
+++ Selenium/index.py
@@ -14,6 +14,22 @@ import sys
 # Chrome Options
 # https://peter.sh/experiments/chromium-command-line-switches/
 
+
+
+def adiciona_repr(cls):
+    def meu_repr(self):
+        class_name = self.__class__.__name__
+        class_dict = self.__dict__
+        class_repr = f'{class_name}({class_dict})'
+        return class_repr
+    cls.__repr__ = meu_repr
+    return cls
+
+@adiciona_repr
+class Time:
+    def __init__(self, nome):
+        self.nome = nome
+
 class Browser:
     ROOT_FOLDER = Path(__file__).parent
     CHROME_DRIVER_PATH = ROOT_FOLDER / 'drivers' / 'chromedriver.exe'
